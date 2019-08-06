aa1645dca4b00c58fac750566d67640acb6512d9
Leon Ji <leon@shoplineapp.com>
2019-08-06 15:06:14 +0800

    feat(user): Use scaffold to generate User calss with fields first name and last name

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190806045755) do
+ActiveRecord::Schema.define(version: 20190806062712) do
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
