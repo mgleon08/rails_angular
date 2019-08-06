6767fcf6136115aa7c20b619f49852db0d76fff7
Leon Ji <leon@shoplineapp.com>
2019-08-06 15:22:14 +0800

    feat(user): Add count API on User collection to return total number of User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -61,6 +61,10 @@ class UsersController < ApplicationController
     end
   end
 
+  def count
+    render json: { count: User.count }
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
