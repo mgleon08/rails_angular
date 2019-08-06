de7240d5033b60e06e0d058660da54d2a92a21e4
Leon Ji <leon@shoplineapp.com>
2019-08-06 18:35:19 +0800

    feat(db): Use Mongodb instead of SQLite

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
-class User < ActiveRecord::Base
+class User
+  include Mongoid::Document
+  field :first_name, type: String
+  field :last_name, type: String
+  field :age, type: Integer
+  field :gender, type: String
+
   validates :first_name, :last_name, presence: true
   validates :age, numericality: { greater_than: 0 }
   validates :gender, inclusion: { in: %w(male female others) }
