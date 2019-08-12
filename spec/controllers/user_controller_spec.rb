3d5a6626a8e961b2e6f6102f85938bfc9a1e36cf
Leon Ji <leon@shoplineapp.com>
2019-08-12 16:11:58 +0800

    feat(test): Add User count and name spec

--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+  render_views
   let(:user){ create(:user) }
 
   let(:params) do
@@ -14,13 +15,15 @@ RSpec.describe UsersController, type: :controller do
   describe 'GET #index' do
     it 'response with status code 200' do
       get :index, format: :json
+      expect(response.content_type).to eq('application/json')
       expect(response).to have_http_status(200)
     end
   end
 
   describe 'GET #show' do
-    it 'when show with a existing user' do
+    it 'response with status code 200' do
       get :show, id: user.id, format: :json
+      expect(response.content_type).to eq('application/json')
       expect(response).to have_http_status(200)
     end
   end
@@ -28,6 +31,7 @@ RSpec.describe UsersController, type: :controller do
   describe 'POST #create' do
     it 'response with status code 200' do
       post :create, user: params
+      expect(response.content_type).to eq('application/json')
       expect(response).to have_http_status(200)
     end
 
@@ -43,6 +47,7 @@ RSpec.describe UsersController, type: :controller do
   describe 'PUT #update' do
     it 'response with status code 200' do
       put :update, id: user.id, user: params
+      expect(response.content_type).to eq('application/json')
       expect(response).to have_http_status(200)
     end
 
@@ -60,12 +65,42 @@ RSpec.describe UsersController, type: :controller do
   describe 'DELETE #destroy' do
     it 'response with status code 200' do
       delete :destroy, id: user.id
+      expect(response.content_type).to eq('application/json')
       expect(response).to have_http_status(200)
     end
 
     it 'when delete with a existing user' do
       new_user = user
       expect{ delete :destroy, id: new_user.id }.to change{ User.count }.by(-1)
+    end
+  end
+
+  describe 'Get #count' do
+    it 'response with status code 200' do
+      get :count
+      expect(response.content_type).to eq('application/json')
+      expect(response).to have_http_status(200)
+    end
+
+    it 'should response with right count number' do
+      new_user = user
+      get :count
+      format_response = JSON.parse(response.body).with_indifferent_access
+      expect(format_response[:count]).to eq(1)
+    end
+  end
+
+  describe 'Get #name' do
+    it 'response with status code 200' do
+      get :name, id: user.id
+      expect(response.content_type).to eq('application/json')
+      expect(response).to have_http_status(200)
+    end
+
+    it 'when response with a existing user' do
+      get :name, id: user.id
+      format_response = JSON.parse(response.body).with_indifferent_access
+      expect(format_response[:name]).to eq("#{user.first_name} #{user.last_name}")
     end
   end
 end
