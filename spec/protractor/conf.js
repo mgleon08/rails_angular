3aa8ce3408628de55f1f16a30ce19edcefd15fac
Leon Ji <leon@shoplineapp.com>
2019-08-12 12:32:11 +0800

    feat(protractor): Add headless mode

--- spec/protractor/conf.js
+++ spec/protractor/conf.js
@@ -1,5 +1,11 @@
 exports.config = {
   framework: 'jasmine',
   seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['userSpec.js']
+  specs: ['userSpec.js'],
+  capabilities: {
+      browserName: 'chrome',
+      chromeOptions: {
+          args: [ "--headless", "--disable-gpu", "--window-size=800,600" ]
+      }
+  }
 }
